7218789ca70439022bd888c238a403c119cdb41e
Lesley Thuto Johnson <whosekid@gmail.com>
2021-10-20 00:16:12 +0200

    Addition of the Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fooderlich/pages/home.dart';
 import 'package:fooderlich/utils/fooderlich_theme.dart';
 
 void main() => runApp(const FooderlichApp());
@@ -10,24 +11,10 @@ class FooderlichApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = FooderLichTheme.dark();
-    // TODO: Apply Home Widget
     return MaterialApp(
       title: "Fooderlich App",
       theme: theme,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Fooderlich",
-            style: theme.textTheme.headline6,
-          ),
-        ),
-        body: Center(
-          child: Text(
-            "Let's get cooking 👨‍🍳",
-            style: theme.textTheme.headline1,
-          ),
-        ),
-      ),
+      home: const Home(),
     );
   }
 }
