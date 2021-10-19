4a05b5d97595370c6e29a16f34fe904e4ab46303
Lesley Thuto Johnson <whosekid@gmail.com>
2021-10-20 00:06:50 +0200

    Theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fooderlich/utils/fooderlich_theme.dart';
 
 void main() => runApp(const FooderlichApp());
 
@@ -8,19 +9,23 @@ class FooderlichApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Create theme
+    final theme = FooderLichTheme.dark();
     // TODO: Apply Home Widget
     return MaterialApp(
-      // TODO: Add theme
       title: "Fooderlich App",
+      theme: theme,
       home: Scaffold(
-        // TODO: Style the title
         appBar: AppBar(
-          title: const Text("Fooderlich"),
+          title: Text(
+            "Fooderlich",
+            style: theme.textTheme.headline6,
+          ),
         ),
-        // TODO: Style the body text
-        body: const Center(
-          child: Text("Let's get cooking 👨‍🍳"),
+        body: Center(
+          child: Text(
+            "Let's get cooking 👨‍🍳",
+            style: theme.textTheme.headline1,
+          ),
         ),
       ),
     );
