88d86570bb3975e03f902feeb09a21ea220a70f6
Lesley Thuto Johnson <whosekid@gmail.com>
2021-12-01 00:39:44 +0200

    Card 3 Decoration

--- lib/widgets/card3.dart
+++ lib/widgets/card3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/utils/fooderlich_theme.dart';
 
 class Card3 extends StatelessWidget {
   const Card3({Key? key}) : super(key: key);
@@ -20,9 +21,105 @@ class Card3 extends StatelessWidget {
         ),
         child: Stack(
           children: [
-            // TODO 5: add dark overlay BoxDecoration
-            // TODO 6: add Container, Column, Icon and Text
+            // dark overlay to make white text pop
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.6),
+                  borderRadius: const BorderRadius.all(Radius.circular(10.0))),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Icon(
+                    Icons.book,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    'Recipe Trends',
+                    style: FooderLichTheme.darkTextTheme.headline2,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  )
+                ],
+              ),
+            ),
             // TODO 7: add Center widget with Chip widget children
+            Center(
+                child: Wrap(
+              alignment: WrapAlignment.start,
+              spacing: 12,
+              children: [
+                Chip(
+                  label: Text(
+                    'Healthy',
+                    style: FooderLichTheme.darkTextTheme.bodyText1,
+                  ),
+                  backgroundColor: Colors.black.withOpacity(0.7),
+                  onDeleted: () {
+                    print('delete');
+                  },
+                ),
+                Chip(
+                  label: Text(
+                    'Vegan',
+                    style: FooderLichTheme.darkTextTheme.bodyText1,
+                  ),
+                  backgroundColor: Colors.black.withOpacity(0.7),
+                  onDeleted: () {
+                    print('delete');
+                  },
+                ),
+                Chip(
+                  label: Text(
+                    'Carrots',
+                    style: FooderLichTheme.darkTextTheme.bodyText1,
+                  ),
+                  backgroundColor: Colors.black.withOpacity(0.7),
+                ),
+                Chip(
+                  label: Text(
+                    'Greens',
+                    style: FooderLichTheme.darkTextTheme.bodyText1,
+                  ),
+                  backgroundColor: Colors.black.withOpacity(0.7),
+                ),
+                Chip(
+                  label: Text(
+                    'Wheat',
+                    style: FooderLichTheme.darkTextTheme.bodyText1,
+                  ),
+                  backgroundColor: Colors.black.withOpacity(0.7),
+                ),
+                Chip(
+                  label: Text(
+                    'Pescetarian',
+                    style: FooderLichTheme.darkTextTheme.bodyText1,
+                  ),
+                  backgroundColor: Colors.black.withOpacity(0.7),
+                ),
+                Chip(
+                  label: Text(
+                    'Mint',
+                    style: FooderLichTheme.darkTextTheme.bodyText1,
+                  ),
+                  backgroundColor: Colors.black.withOpacity(0.7),
+                ),
+                Chip(
+                  label: Text(
+                    'Lemongrass',
+                    style: FooderLichTheme.darkTextTheme.bodyText1,
+                  ),
+                  backgroundColor: Colors.black.withOpacity(0.7),
+                ),
+              ],
+            )),
           ],
         ),
       ),
